ca89cb47f9b56b62c16235731fda38c8760cf7d7
Michael Zekoff <zekoff@gmail.com>
2014-11-20 06:21:27 +0000

    Add left/right movement.

--- tetris.js
+++ tetris.js
@@ -3,13 +3,13 @@ var Tetris = Tetris || {};
 
 Tetris.COLUMNS = 10;
 Tetris.ROWS = 18;
-Tetris.TILE_SIZE = 32; // square
-Tetris.tickLength = 1000;
+Tetris.TILE_SIZE = 32; // pixels square
+Tetris.tickLength = 1000; // milliseconds
 var game = new Phaser.Game(Tetris.COLUMNS * Tetris.TILE_SIZE,
     Tetris.ROWS * Tetris.TILE_SIZE, Phaser.AUTO, '',
     { preload: preload, create: create, update: update }, true);
 var actionQueue = [];
-var counter = 0;
+var counter = 0; // milliseconds
 
 function preload() {
     game.load.image('block','block.png');
@@ -17,6 +17,30 @@ function preload() {
 
 function create() {
     Tetris.activeTetrad = game.add.sprite(0,0,'block');
+    var leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+    leftKey.onDown.add(function() {
+        actionQueue.push(function() {
+            Tetris.activeTetrad.x -= Tetris.TILE_SIZE;
+        });
+    });
+    var rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+    rightKey.onDown.add(function() {
+        actionQueue.push(function() {
+            Tetris.activeTetrad.x += Tetris.TILE_SIZE;
+        });
+    });
+    var upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+    upKey.onDown.add(function() {
+        actionQueue.push(function() {
+            // rotate tetrad
+        });
+    });
+    var downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+    downKey.onDown.add(function() {
+        actionQueue.push(function() {
+            // drop tetrad 
+        });
+    });
 }
 
 function update() {
@@ -31,6 +55,6 @@ function update() {
 
 function processTick() {
     actionQueue.push(function() {
-        Tetris.activeTetrad.y += 32;
+        Tetris.activeTetrad.y += Tetris.TILE_SIZE;
     });
 }
