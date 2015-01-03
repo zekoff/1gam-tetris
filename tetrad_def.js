078ef384fe13de87e7d6fe1dc45d291c6db51e8f
Michael Zekoff <zekoff@gmail.com>
2015-01-03 18:09:55 +0000

    Bring vars into Tetris namespace.

---
+++ tetrad_def.js
@@ -0,0 +1,142 @@
+var _ = _ || {};
+var Tetris = Tetris || {};
+
+Tetris.Tetrad = function() {
+    this.x = 0;
+    this.y = 0;
+    this.createRandom = function() {
+        this.matrix = []; // 2D matrix, access by [row][column]
+        for (var i = 0; i < 4; i++) this.matrix.push([0, 0, 0, 0]);
+        switch (_.random(6)) {
+            case 0:
+                /*
+                0 0 0 0
+                1 1 0 0
+                1 0 0 0
+                1 0 0 0
+                */
+                this.matrix[1][0] = 1;
+                this.matrix[1][1] = 1;
+                this.matrix[2][0] = 1;
+                this.matrix[3][0] = 1;
+                break;
+            case 1:
+                /*
+                0 0 0 0
+                1 1 0 0
+                0 1 0 0
+                0 1 0 0
+                */
+                this.matrix[1][0] = 1;
+                this.matrix[1][1] = 1;
+                this.matrix[2][1] = 1;
+                this.matrix[3][1] = 1;
+                break;
+            case 2:
+                /*
+                1 0 0 0
+                1 0 0 0
+                1 0 0 0
+                1 0 0 0
+                */
+                this.matrix[0][0] = 1;
+                this.matrix[1][0] = 1;
+                this.matrix[2][0] = 1;
+                this.matrix[3][0] = 1;
+                break;
+            case 3:
+                /*
+                0 0 0 0
+                0 1 0 0
+                1 1 0 0
+                1 0 0 0
+                */
+                this.matrix[1][1] = 1;
+                this.matrix[2][0] = 1;
+                this.matrix[2][1] = 1;
+                this.matrix[3][0] = 1;
+                break;
+            case 4:
+                /*
+                0 0 0 0
+                1 0 0 0
+                1 1 0 0
+                0 1 0 0
+                */
+                this.matrix[1][0] = 1;
+                this.matrix[2][0] = 1;
+                this.matrix[2][1] = 1;
+                this.matrix[3][1] = 1;
+                break;
+            case 5:
+                /*
+                0 0 0 0
+                0 0 0 0
+                1 1 0 0
+                1 1 0 0
+                */
+                this.matrix[2][0] = 1;
+                this.matrix[2][1] = 1;
+                this.matrix[3][0] = 1;
+                this.matrix[3][1] = 1;
+                break;
+            case 6:
+                /*
+                0 0 0 0
+                0 0 0 0
+                0 1 0 0
+                1 1 1 0
+                */
+                this.matrix[2][1] = 1;
+                this.matrix[3][0] = 1;
+                this.matrix[3][1] = 1;
+                this.matrix[3][2] = 1;
+                break;
+        }
+        return this;
+    };
+    this.log = function() {
+        for (var row = 0; row < 4; row++) {
+            var line = "";
+            for (var col = 0; col < 4; col++) {
+                if (this.matrix[row][col]) line += "X ";
+                else line += "  ";
+            }
+            console.log(line);
+        }
+    };
+    this.rotateRight = function() {
+        // rotate outer layer
+        for (var i = 0; i < 3; i++) {
+            // save top row
+            var temp = this.matrix[0][i];
+            // left column to top row
+            this.matrix[0][i] = this.matrix[3 - i][0];
+            // bottom row to left column
+            this.matrix[3 - i][0] = this.matrix[3][3 - i];
+            // right column to bottom row
+            this.matrix[3][3 - i] = this.matrix[i][3];
+            // top row (temp) to right columm
+            this.matrix[i][3] = temp;
+        }
+        // rotate inner layer
+        var innerTemp = this.matrix[1][1];
+        this.matrix[1][1] = this.matrix[2][1];
+        this.matrix[2][1] = this.matrix[2][2];
+        this.matrix[2][2] = this.matrix[1][2];
+        this.matrix[1][2] = innerTemp;
+        // fill bottom rows
+        while (_.isEqual(this.matrix[3], [0, 0, 0, 0])) {
+            for (var i = 2; i >= 0; i--) this.matrix[i + 1] = this.matrix[i];
+            this.matrix[0] = [0, 0, 0, 0];
+        }
+        return this;
+    };
+    this.rotateLeft = function() {
+        for (var i = 0; i < 3; i++) this.rotateRight();
+        return this;
+    };
+    this.clone = function() {
+        return _.cloneDeep(this);
+    };
+};
--- tetris.js
+++ tetris.js
@@ -1,159 +1,21 @@
 var Phaser = Phaser || {};
 var Tetris = Tetris || {};
 var _ = _ || {};
-
-function Tetrad() {
-    this.x = 0;
-    this.y = 0;
-    this.createRandom = function() {
-        this.matrix = []; // 2D matrix, access by [row][column]
-        for (var i = 0; i < 4; i++) this.matrix.push([0, 0, 0, 0]);
-        switch (_.random(6)) {
-            case 0:
-                /*
-                0 0 0 0
-                1 1 0 0
-                1 0 0 0
-                1 0 0 0
-                */
-                this.matrix[1][0] = 1;
-                this.matrix[1][1] = 1;
-                this.matrix[2][0] = 1;
-                this.matrix[3][0] = 1;
-                break;
-            case 1:
-                /*
-                0 0 0 0
-                1 1 0 0
-                0 1 0 0
-                0 1 0 0
-                */
-                this.matrix[1][0] = 1;
-                this.matrix[1][1] = 1;
-                this.matrix[2][1] = 1;
-                this.matrix[3][1] = 1;
-                break;
-            case 2:
-                /*
-                1 0 0 0
-                1 0 0 0
-                1 0 0 0
-                1 0 0 0
-                */
-                this.matrix[0][0] = 1;
-                this.matrix[1][0] = 1;
-                this.matrix[2][0] = 1;
-                this.matrix[3][0] = 1;
-                break;
-            case 3:
-                /*
-                0 0 0 0
-                0 1 0 0
-                1 1 0 0
-                1 0 0 0
-                */
-                this.matrix[1][1] = 1;
-                this.matrix[2][0] = 1;
-                this.matrix[2][1] = 1;
-                this.matrix[3][0] = 1;
-                break;
-            case 4:
-                /*
-                0 0 0 0
-                1 0 0 0
-                1 1 0 0
-                0 1 0 0
-                */
-                this.matrix[1][0] = 1;
-                this.matrix[2][0] = 1;
-                this.matrix[2][1] = 1;
-                this.matrix[3][1] = 1;
-                break;
-            case 5:
-                /*
-                0 0 0 0
-                0 0 0 0
-                1 1 0 0
-                1 1 0 0
-                */
-                this.matrix[2][0] = 1;
-                this.matrix[2][1] = 1;
-                this.matrix[3][0] = 1;
-                this.matrix[3][1] = 1;
-                break;
-            case 6:
-                /*
-                0 0 0 0
-                0 0 0 0
-                0 1 0 0
-                1 1 1 0
-                */
-                this.matrix[2][1] = 1;
-                this.matrix[3][0] = 1;
-                this.matrix[3][1] = 1;
-                this.matrix[3][2] = 1;
-                break;
-        }
-        return this;
-    };
-    this.log = function() {
-        for (var row = 0; row < 4; row++) {
-            var line = "";
-            for (var col = 0; col < 4; col++) {
-                if (this.matrix[row][col]) line += "X ";
-                else line += "  ";
-            }
-            console.log(line);
-        }
-    };
-    this.rotateRight = function() {
-        // rotate outer layer
-        for (var i = 0; i < 3; i++) {
-            // save top row
-            var temp = this.matrix[0][i];
-            // left column to top row
-            this.matrix[0][i] = this.matrix[3 - i][0];
-            // bottom row to left column
-            this.matrix[3 - i][0] = this.matrix[3][3 - i];
-            // right column to bottom row
-            this.matrix[3][3 - i] = this.matrix[i][3];
-            // top row (temp) to right columm
-            this.matrix[i][3] = temp;
-        }
-        // rotate inner layer
-        var innerTemp = this.matrix[1][1];
-        this.matrix[1][1] = this.matrix[2][1];
-        this.matrix[2][1] = this.matrix[2][2];
-        this.matrix[2][2] = this.matrix[1][2];
-        this.matrix[1][2] = innerTemp;
-        // fill bottom rows
-        while (_.isEqual(this.matrix[3], [0, 0, 0, 0])) {
-            for (var i = 2; i >= 0; i--) this.matrix[i + 1] = this.matrix[i];
-            this.matrix[0] = [0, 0, 0, 0];
-        }
-        return this;
-    };
-    this.rotateLeft = function() {
-        for (var i = 0; i < 3; i++) this.rotateRight();
-        return this;
-    };
-    this.clone = function() {
-        return _.cloneDeep(this);
-    };
-}
+var Tetrad = Tetris.Tetrad;
 
 Tetris.COLUMNS = 10;
 Tetris.ROWS = 18;
 Tetris.TILE_SIZE = 33; // pixels square
 Tetris.tickLength = 990; // milliseconds
 Tetris.field = [];
+Tetris.counter = 0; // milliseconds
+Tetris.renderBlocks = [];
 var game = new Phaser.Game(Tetris.COLUMNS * Tetris.TILE_SIZE,
     Tetris.ROWS * Tetris.TILE_SIZE, Phaser.AUTO, '', {
         preload: preload,
         create: create,
         update: update
     }, true);
-var counter = 0; // milliseconds
 
 function preload() {
     game.load.image('block', 'block.png');
@@ -190,32 +52,33 @@ function create() {
     var downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
     downKey.onDown.add(function() {
         processTick();
-        // counter = Tetris.tickLength + 1;
     });
 }
 
-var blocks = [];
-
 function update() {
-    blocks.forEach(function(element) {
+    Tetris.counter += game.time.elapsed;
+    if (Tetris.counter > Tetris.tickLength)
+        processTick();
+    render();
+}
+
+function render() {
+    Tetris.renderBlocks.forEach(function(element) {
         element.destroy();
     });
-    blocks = [];
-    counter += game.time.elapsed;
-    if (counter > Tetris.tickLength)
-        processTick();
+    Tetris.renderBlocks = [];
     for (var row = 0; row < 4; row++)
         for (var col = 0; col < 4; col++)
             if (Tetris.activeTetrad.matrix[row][col])
-                blocks.push(game.add.image((col + Tetris.activeTetrad.x) * Tetris.TILE_SIZE, (row + Tetris.activeTetrad.y) * Tetris.TILE_SIZE, 'block'));
+                Tetris.renderBlocks.push(game.add.image((col + Tetris.activeTetrad.x) * Tetris.TILE_SIZE, (row + Tetris.activeTetrad.y) * Tetris.TILE_SIZE, 'block'));
     for (var fieldRow = 0; fieldRow < Tetris.ROWS; fieldRow++)
         for (var fieldCol = 0; fieldCol < Tetris.COLUMNS; fieldCol++)
             if (Tetris.field[fieldRow][fieldCol])
-                blocks.push(game.add.image(fieldCol * Tetris.TILE_SIZE, fieldRow * Tetris.TILE_SIZE, 'block'));
+                Tetris.renderBlocks.push(game.add.image(fieldCol * Tetris.TILE_SIZE, fieldRow * Tetris.TILE_SIZE, 'block'));
 }
 
 function processTick() {
-    counter = 0;
+    Tetris.counter = 0;
     var copy = Tetris.activeTetrad.clone();
     copy.y += 1;
     if (!checkCollision(copy))
